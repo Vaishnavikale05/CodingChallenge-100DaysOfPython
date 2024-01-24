6ec5f921d20a3da84ac07172c15f8a255bd1ae1f
Vaishnavikale05 <157374843+Vaishnavikale05@users.noreply.github.com>
2024-01-24 16:01:02 +0530

    Add files via upload

---
+++ Day 9/Converter.py
@@ -0,0 +1,12 @@
+#Converting kilometers to miles
+
+km=float(input("Enter value in km :"))
+
+miles=km*0.621371
+
+print("Value in miles is:",miles)
+
+#converting meters to km
+m=float(input("Enter value in meters :"))
+km=m/1000
+print(m," in kilometers is :",km)
